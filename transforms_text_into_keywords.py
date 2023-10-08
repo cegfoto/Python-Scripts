47d7180796ee3b38c0d342176957c50f286f9054
Gerson Mauri <cegfoto@gmail.com>
2023-10-08 12:43:05 -0300

    first commit

---
+++ transforms_text_into_keywords.py
@@ -0,0 +1,22 @@
+removeSentences = [
+    "of",
+    "the",
+    "and",
+    "in",
+    "with",
+    "above",
+    "among",
+    "on",
+    "a",
+    "under"
+]
+
+text = input("Input the text")
+text = text.replace(',', '')
+text = text.replace('.', '')
+listText = text.split(' ')
+listText = list(dict.fromkeys(listText))
+for word in listText:
+    if word in removeSentences:
+        listText.remove(word)
+print(';'.join(listText))
