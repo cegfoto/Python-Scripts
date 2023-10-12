26109ba4b0cf35f618397dfb8bdeb0d787003f85
Gerson Mauri <cegfoto@gmail.com>
2023-10-12 19:21:14 -0300

    melhorar execucao

--- transforms_text_into_keywords.py
+++ transforms_text_into_keywords.py
@@ -8,18 +8,26 @@ removeSentences = [
     "among",
     "on",
     "a",
-    "under"
+    "under",
+    "selective",
+    "over"
 ]
 
-text = input("Input the text:")
-text = text.replace(',', '')
-text = text.replace('.', '')
-listText = text.split(' ')
-listText = list(dict.fromkeys(listText))
-toRemove = []
-for word in listText:
-    if word in removeSentences:
-        toRemove.append(word)
-for remove in toRemove:
-    listText.remove(remove)
-print(';'.join(listText))
+loop = 1
+while loop:
+	text = input("Input the text:")
+	if not text:
+		loop = 0
+		continue
+	text = text.replace(',', '')
+	text = text.replace('.', '')
+	listText = text.split(' ')
+	listText = list(dict.fromkeys(listText))
+	toRemove = []
+	for word in listText:
+		if word in removeSentences:
+			toRemove.append(word)
+	for remove in toRemove:
+		listText.remove(remove)
+	print(';'.join(listText))
+print('operação finalizada')
